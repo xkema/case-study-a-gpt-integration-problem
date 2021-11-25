55ed825ecad77e2a0f42cf659c85507bcecea091
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2021-11-25 14:55:36 +0300

    Included a slot response console logger script block
    - Updated mock module with "slotRenderEnded" event and "enableLazyLoad" feature
    - Pointed recommended path in directory page

--- docs/assets/gpt-mock.js
+++ docs/assets/gpt-mock.js
@@ -2,6 +2,8 @@
 if((window['H0rWrXR3crg'] && window['H0rWrXR3crg']['gptMockEnabled'] === true)  || window.location.search.split(/[?&]/).findIndex((queryParam) => /^mock=(?:true|1)$/.test(queryParam)) !== -1) {
   // a reference to the page slots
   const __slots = [];
+  // a reference to the slot events
+  const __events = [];
   // "pubads_impl_***.js" library may evaluate this mock with non-mock objects if, mock module is enabled but there is no ad blocker usage
   const gptMock = {
     cmd: {
@@ -19,7 +21,17 @@ if((window['H0rWrXR3crg'] && window['H0rWrXR3crg']['gptMockEnabled'] === true)  
       __slots.push(slot);
       return slot;
     },
-    pubads: () => {},
+    pubads: () => {
+      return {
+        addEventListener: (eventType, listener) => {
+          __events.push({
+            eventType: eventType,
+            listener: listener
+          });
+        },
+        enableLazyLoad: (config) => { /* config: { fetchMarginPercent: 100, renderMarginPercent: 50, mobileScaling: 2.0 } */ }
+      };
+    },
     enableServices: () => {},
     display: (divOrSlot) => {
       const slot = __slots.find(slot => slot.getSlotElementId() === divOrSlot);
@@ -35,7 +47,7 @@ if((window['H0rWrXR3crg'] && window['H0rWrXR3crg']['gptMockEnabled'] === true)  
         slot.__size = 'unexpected_size';
       }
       const slotElement = document.createElement('div');
-      slotElement.style.cssText = `display: flex; flex-direction: column; justify-content: center; margin: 0 auto; background-color: blue; min-height: 50px;`;
+      slotElement.style.cssText = `display: flex; flex-direction: column; justify-content: center; margin: 0 auto; background-color: blue; min-height: 50px; opacity: 0; transition: opacity 0.5s ease 0.5s;`;
       if(slot.__size !== 'unexpected_size') {
         slotElement.style.cssText += `width: ${slot.__size[0]}px; height: ${slot.__size[1]}px;`;
       }
@@ -46,11 +58,22 @@ if((window['H0rWrXR3crg'] && window['H0rWrXR3crg']['gptMockEnabled'] === true)  
         slot.getSlotElementId()
       ].forEach((infoItem) => {
         const slotInfoElement = document.createElement('span');
-        slotInfoElement.innerText = `${infoItem.toString()}`;
+        slotInfoElement.innerText = Array.isArray(infoItem) ? infoItem.join('x') : infoItem;
         slotElement.insertAdjacentElement('beforeEnd', slotInfoElement);
       });
       // insert mock slot
       document.querySelector(`#${divOrSlot}`).insertAdjacentElement('beforeEnd', slotElement);
+      // animate display activity
+      setTimeout(() => { slotElement.style.opacity = 1; }, 0);
+      // release render event for the slot
+      const slotRenderEndedEvent = __events.find(event => event.eventType === 'slotRenderEnded');
+      if(slotRenderEndedEvent) {
+        slotRenderEndedEvent.listener.call(null, {
+          isEmpty: false,
+          slot: slot,
+          size: slot.__size
+        })
+      }
     }
   };
   // replace "googletag" object with mock
